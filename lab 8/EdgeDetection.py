3c2b81dfa2357feb2be57a9b88c9382cf1ae266a
kkmm1234 <keithmoffitt2001@gmail.com>
2024-08-27 20:39:28 +0100

    L8 ploted 3x3

--- lab 8/EdgeDetection.py
+++ lab 8/EdgeDetection.py
@@ -6,7 +6,7 @@ img = cv2.imread('ATU.jpg')
 
 # Rows and Columns
 nrows = 2
-ncols = 1
+ncols = 2
 
 #gray scale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -16,7 +16,7 @@ rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 blur3x3 = cv2.GaussianBlur(gray,(3, 3),0)
 blur13x13 = cv2.GaussianBlur(gray,(13, 13),0)
 
-
+#Atu image plot
 #original image
 plt.subplot(nrows, ncols,1),plt.imshow(rgb, cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
@@ -24,5 +24,9 @@ plt.title('Original'), plt.xticks([]), plt.yticks([])
 # GreyScale image
 plt.subplot(nrows, ncols,2),plt.imshow(gray, cmap = 'gray')
 plt.title('Graycale'), plt.xticks([]), plt.yticks([])
+
+# Blur 3x3
+plt.subplot(nrows, ncols,3),plt.imshow(blur3x3, cmap = 'gray')
+plt.title('Blur 3x3'), plt.xticks([]), plt.yticks([])
 
 plt.show()
