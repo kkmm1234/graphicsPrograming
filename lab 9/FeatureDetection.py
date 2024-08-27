93a603ce591536060f68cc8d8fb67459aa52bd41
kkmm1234 <keithmoffitt2001@gmail.com>
2024-08-27 22:57:47 +0100

    L9 added image matching to atu and own imgs

--- lab 9/FeatureDetection.py
+++ lab 9/FeatureDetection.py
@@ -6,6 +6,8 @@ img = cv2.imread('ATU1.jpg')
 imgHarris = img.copy()
 imgShiTomasi = img.copy()
 
+img2 = cv2.imread('ATU2.jpg')
+
 # Rows and Columns
 nrows = 2
 ncols = 2
@@ -40,6 +42,22 @@ kp1, des1 = orb.compute(img, kp)
 imgOrb = cv2.drawKeypoints(img, kp, None, color=(0,255,0), flags=0)
 
 
+# Image Matching
+gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+kp2, des2 = orb.detectAndCompute(img2,None)
+
+# create BFMatcher object
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+# Match descriptors.
+matches = bf.match(des1,des2)
+# Sort them in the order of their distance.
+matches = sorted(matches, key = lambda x:x.distance)
+# Draw first 10 matches.
+img3 = cv2.drawMatches(img,kp1,img2,kp2,matches[:10],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+
 # GreyScale image
 plt.subplot(nrows, ncols,1),plt.imshow(gray, cmap = 'gray')
 plt.title('Graycale'), plt.xticks([]), plt.yticks([])
@@ -55,6 +73,11 @@ plt.title('ShiTomasi'), plt.xticks([]), plt.yticks([])
 # Orb Detection
 plt.subplot(nrows, ncols,4),plt.imshow(cv2.cvtColor(imgOrb, cv2.COLOR_BGR2RGB), cmap = 'gray')
 plt.title('Orb'), plt.xticks([]), plt.yticks([])
+
+# Image Matching
+plt.figure(figsize=(6, 6))
+plt.imshow(cv2.cvtColor(img3, cv2.COLOR_BGR2RGB), cmap = 'gray')
+plt.title('Image Matching ATU'), plt.xticks([]), plt.yticks([])
 
 
 plt.show()
